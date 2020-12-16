4388f5629a1b9c0fa0c1f81f8df392a2911bb872
Louis <szxc12123>
2020-12-16 19:34:39 +0800

    繪製坦克

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,5 +1,26 @@
-public class GameClient {
-    public static void main(String[] args) {
-        System.out.println("Tank War!");
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+
+    private int screenWidth;
+    private int screenHeight;
+
+GameClient(){
+    this(800,600);          //設置遊戲畫面大小
+}
+public GameClient(int screenWidth,int screenHeight){
+    this.screenWidth = screenWidth;
+    this.screenHeight = screenHeight;
+    this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+}
+
+    @Override
+    protected void paintComponent(Graphics g) {                 //繪製功能
+        super.paintComponent(g);                        //可有可無
+        g.drawImage(new ImageIcon("assets\\images\\iTankD.png")
+                .getImage(),400,300,null);                  //繪製圖案
     }
+
+
 }
